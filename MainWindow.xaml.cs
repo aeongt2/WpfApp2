21550cbdd878d1af5a1ab2727850a3d976489a98
Muhammad Ahmad <unknowndonlion@gmail.com>
2022-07-05 22:54:48 +0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Windows;
+
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+    }
+}
